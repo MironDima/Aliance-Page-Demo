b04fa848495d176a0bd33f378cd39a12005e991d
Miron Dima <mirontsov2098@mail.ru>
2023-09-04 22:35:18 +0300

    add fix bug

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('ji');
+const navbar = document.querySelector('.navbar');
+window.addEventListener('scroll', () => {
+const scrollTopPosition = window.scrollY
+const logoLinkLight = navbar.querySelector('.logo-img-light')
+const logoLinkBlack = navbar.querySelector('.logo-img-black')
+
+if( scrollTopPosition > 150) {
+	navbar.classList.add('navbar-scroll')
+	logoLinkLight.classList.add('hidden')
+	logoLinkBlack.classList.remove('hidden')
+}else {
+	navbar.classList.remove('navbar-scroll')
+	logoLinkBlack.classList.add('hidden')
+	logoLinkLight.classList.remove('hidden')
+}
+})
+
