18f00214e58cdd90f45bf922dad057f79dbc0bf9
Miron Dima <mirontsov2098@mail.ru>
2023-09-10 18:04:15 +0300

    add svg-logo

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 const navbar = document.querySelector('.navbar');
 window.addEventListener('scroll', () => {
 const scrollTopPosition = window.scrollY
-const logoLinkLight = navbar.querySelector('.logo-img-light')
-const logoLinkBlack = navbar.querySelector('.logo-img-black')
+const svgLogo = document.querySelector('.logo-svg>use')
+
 
 if( scrollTopPosition > 150) {
 	navbar.classList.add('navbar-scroll')
-	logoLinkLight.classList.add('hidden')
-	logoLinkBlack.classList.remove('hidden')
+	svgLogo.href.baseVal = 'img/iconSprite.svg#logo2';
 }else {
 	navbar.classList.remove('navbar-scroll')
-	logoLinkBlack.classList.add('hidden')
-	logoLinkLight.classList.remove('hidden')
+	svgLogo.href.baseVal = 'img/iconSprite.svg#logo-light';
 }
 })
 
