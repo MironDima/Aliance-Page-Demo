24480faadecef6fc642b514029c1a2c8de782818
Miron Dima <mirontsov2098@mail.ru>
2023-09-20 23:17:46 +0300

    fix mobail first

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const navbar = document.querySelector('.navbar');
 window.addEventListener('scroll', () => {
 const scrollTopPosition = window.scrollY
-const svgLogo = document.querySelector('.logo-svg>use')
-
+const svgLogo = document.querySelector('.logo-img-light>use')
+const mobailMenuLine = document.querySelectorAll('.mobail-menu-line')
 
 if( scrollTopPosition > 150) {
 	navbar.classList.add('navbar-scroll')
 	svgLogo.href.baseVal = 'img/iconSprite.svg#logo2';
+	mobailMenuLine.forEach((mobailMenuLineItem) => {
+		mobailMenuLineItem.style.backgroundColor = 'black'})
+
 }else {
 	navbar.classList.remove('navbar-scroll')
 	svgLogo.href.baseVal = 'img/iconSprite.svg#logo-light';
+	mobailMenuLine.forEach((mobailMenuLineItem) => {
+		mobailMenuLineItem.style.backgroundColor = ''})
 }
 })
 
